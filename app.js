a6ab3eda9d804cca00a310864e5371354b63e56e
Muhammad Saddam <saddam.fastnu@gmail.com>
2023-10-25 12:12:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,83 +2,74 @@ const express = require('express');
 const session = require('express-session');
 const redis = require('redis');
 const connectRedis = require('connect-redis');
-const bodyParser = require('body-parser');  // Required to parse POST requests
-
-const RedisStore = connectRedis(session);
-
-const HOST = process.env.REDIS_HOST || 'node-cluster.gcds8v.ng.0001.euw1.cache.amazonaws.com';
-const PORT = process.env.REDIS_PORT || 6379;
-const SECRET = process.env.SESSION_SECRET || 'default-secret';
+const bodyParser = require('body-parser');
+const ip = require("ip");
 
 const app = express();
-const redisClient = redis.createClient({ host: HOST, port: PORT });
+app.use(bodyParser.urlencoded({ extended: true }));
 
-redisClient.on('error', (error) => {
-  console.error(`Redis error: ${error}`);
+app.set('trust proxy', 1);
+const RedisStore = connectRedis(session);
+
+const primary_endpoint = process.env.PRIMARY_ENDPOINT;
+
+const redisClient = redis.createClient({
+  host: primary_endpoint,
+  port: 6379
 });
 
-redisClient.on('connect', () => {
-  console.log('Connected to Redis.');
+redisClient.on('error', function (err) {
+  console.log('Could not establish a connection with Redis. ' + err);
 });
 
-redisClient.on('reconnecting', () => {
-  console.log('Reconnecting to Redis...');
+redisClient.on('connect', function (err) {
+  console.log('Connected to Redis successfully.');
 });
 
-redisClient.on('end', () => {
-  console.log('Redis connection closed.');
-});
-
-const sessionMiddleware = session({
+app.use(session({
   store: new RedisStore({ client: redisClient }),
-  secret: SECRET,
+  secret: 'secret_educative_string',
   resave: false,
   saveUninitialized: false,
-});
+  cookie: {
+      secure: false,
+      httpOnly: false,
+      maxAge: 1000 * 60 * 10
+  }
+}));
 
-app.use(sessionMiddleware);
-app.use(bodyParser.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 3000;
 
-// Root route with form to get value
 app.get('/', (req, res) => {
-  res.send(`
-    <form action="/store" method="post">
-      <input type="text" name="value" placeholder="Enter a value" />
-      <button type="submit">Save</button>
+  let form = `
+  <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh; background-color: #F3F3F3; font-family: Arial, sans-serif;">
+    <form action="/set_custom_session" method="POST" style="padding: 30px; border: 1px solid #e0e0e0; background-color: #FFFFFF; border-radius: 5px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width: 350px;">
+      <label for="customSession" style="color: #3498DB; font-size: 1.5em;">Enter Custom Session Value:</label><br>
+      <input type="text" id="customSession" name="customSession" placeholder="Enter session value" style="margin-top: 20px; padding: 10px; width: 100%; font-size: 1.2em; border-radius: 3px;"><br>
+      <input type="submit" value="Submit" style="margin-top: 30px; padding: 10px 20px; background-color: #2ECC71; border: none; color: white; cursor: pointer; font-size: 1.2em; border-radius: 3px;">
     </form>
-    <br />
-    <div>
-      Stored value in Redis: ${req.session.value || 'No value stored yet.'}
+    <div style="color: #7F8C8D; margin-top: 30px; background-color: #FFFFFF; padding: 20px; border-radius: 5px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width: 350px; text-align: center;">
+      Host IP: ${ip.address()}<br>
+      Custom Session Value: ${req.session.customSession || 'Not set'}
     </div>
-    <br />
-    <a href="/clear-session">Clear Session</a>
-  `);
+    <a href="/clear_custom_session" style="color: #E74C3C; text-decoration: none; margin-top: 20px; font-size: 1.2em;">Clear Session</a>
+  </div>`;
+  
+  res.send(form);
 });
 
-// POST route to store value in Redis session
-app.post('/store', (req, res) => {
-  req.session.value = req.body.value;  // Store the value in Redis session
-  res.redirect('/');  // Redirect back to root route
+app.post('/set_custom_session', (req, res) => {
+  req.session.customSession = req.body.customSession;
+  res.redirect('/');
 });
 
-// Endpoint to clear the session
-app.get('/clear-session', (req, res) => {
-  req.session.destroy((err) => {
+app.get('/clear_custom_session', (req, res) => {
+  req.session.destroy(err => {
     if (err) {
-      console.error("Failed to clear session:", err);
-      res.status(500).send('Failed to clear session');
-      return;
+      return res.redirect('/');
     }
     res.redirect('/');
   });
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-const SERVER_PORT = 3000;
-app.listen(SERVER_PORT, () => {
-  console.log(`Server is running on port ${SERVER_PORT}`);
-});
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
